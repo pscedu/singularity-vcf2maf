3480b9e97c913b495a76be1170f6c95f716feb75
Ivan Cao-Berg <icaoberg@psc.edu>
2021-07-21 12:28:15 -0400

    Fixed modulefile

--- 1.6.21/modulefile.lua
+++ 1.6.21/modulefile.lua
@@ -10,33 +10,26 @@ whatis("Name: vcf2maf")
 whatis("Version: 1.6.21")
 whatis("Category: Biological Sciences")
 whatis("URL: https://www.psc.edu/resources/software")
-whatis("Description: vcf2maf is a tool Converts a VCF into a MAF, where each variant is annotated to only one of all possible gene isoforms.")
+whatis("Description: Convert a VCF into a MAF, where each variant is annotated to only one of all possible gene isoforms")
 
 help([[
-vcf2maf is a tool Converts a VCF into a MAF, where each variant is annotated to only one of all possible gene isoforms.
-  
+Convert a VCF into a MAF, where each variant is annotated to only one of all possible gene isoforms.
+
 To load the module type
-  
+
 > module load vcf2maf/1.6.21
-  
+
 To unload the module type
-  
+
 > module unload vcf2maf/1.6.21
-  
+
 Documentation
 -------------
-http://www.htslib.org/doc/#manual-pages
-  
-For help, type
-  
-> vcf2maf --help
-  
-Repository
-----------
-https://github.com/pscedu/singularity-vcf2maf
-  
 Tools included in this module are
 * vcf2maf
+* maf2vcf
+* vcf2vcf
+* maf2maf
 ]])
 
 local package = "vcf2maf"
